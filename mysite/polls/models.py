582028b93276492f71f0e365e479d8786bc699ff
Michael Lewis <michaeladamlewis@gmail.com>
2015-04-24 19:49:09 -0700

    issue solved where i had no question properties

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 
 class Question(models.Model):
-    # ...
+    question_text = models.CharField(max_length=200)
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
         
