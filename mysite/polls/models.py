1446910e6f1cad10f519ed9d6cfd0fd5c53e7dd3
Michael Lewis <michaeladamlewis@gmail.com>
2015-04-24 20:57:24 -0700

    check in

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,9 +5,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
-    return self.question_text
+        return self.question_text
     def was_published_recently(self):
-    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
